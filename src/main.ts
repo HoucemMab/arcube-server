e7f772d310044438b0d38609f50b5ccab4ee2240
HoucemUnty <houssam@unty.fr>
2025-02-01 13:35:15 +0100

    add cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000',"https://arcube-hcm.netlify.app/"],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
